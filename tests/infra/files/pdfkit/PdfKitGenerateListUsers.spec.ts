e862d5613f7d8002a7878fd295cb49a9d86b8c82
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-09-18 16:57:47 -0300

    test: send throw if PdfKitGenerateListUsers throws

--- tests/infra/files/pdfkit/PdfKitGenerateListUsers.spec.ts
+++ tests/infra/files/pdfkit/PdfKitGenerateListUsers.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { User } from '../../../../src/domain/entities/User'
 import { Profile } from '../../../../src/domain/valueObjects/Profile'
 import { PdfKitGenerateListUsers } from '../../../../src/infra/files/pdfkit/PdfKitGenerateListUsers'
@@ -21,5 +21,22 @@ describe('PdfKitGenerateListUsers', () => {
     const buffer = await sut.generate([userOne, userTwo])
 
     expect(buffer).toBeInstanceOf(Buffer)
+  })
+
+  it('should throw if PdfKitGenerateListUsers throws', async () => {
+    const sut = new PdfKitGenerateListUsers()
+    const userOne = new User({
+      name: 'anyName',
+      age: 20,
+      profile: new Profile('anyPhotoUrl', 'anyLinkedInUrl')
+    })
+
+    vi.spyOn(userOne, 'getProfile').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.generate([userOne])
+
+    await expect(promise).rejects.toThrow()
   })
 })
