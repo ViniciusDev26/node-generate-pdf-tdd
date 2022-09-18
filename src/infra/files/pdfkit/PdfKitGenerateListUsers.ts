87bf173b5a3e132208f2432f27942daa116b202a
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-09-18 16:58:28 -0300

    refactor: move try/catch to early file and ensure return error if throws

--- src/infra/files/pdfkit/PdfKitGenerateListUsers.ts
+++ src/infra/files/pdfkit/PdfKitGenerateListUsers.ts
@@ -8,32 +8,31 @@ import { randomUUID } from 'crypto'
 export class PdfKitGenerateListUsers implements GenerateListUsersPdf {
   async generate (users: User[]): Promise<Buffer> {
     const pathFile = resolve(__dirname, '..', '..', 'tmp', `${randomUUID()} - listFile.pdf`)
-
-    const stream = createWriteStream(pathFile)
-    const document = new Pdfkit()
-    document.pipe(stream)
-
-    document.text('users:')
-
-    for (const user of users) {
-      document.text(`name: ${user.getName()}`)
-      document.text(`linkedIn: ${user.getProfile().getLinkedInUrl()}`)
-    }
-
-    document.end()
-
     return await new Promise((resolve, reject) => {
-      stream
-        .on('error', (err) => {
-          unlinkSync(pathFile)
-          reject(err)
-        })
-        .on('finish', () => {
+      try {
+        const stream = createWriteStream(pathFile)
+        const document = new Pdfkit()
+        document.pipe(stream)
+
+        document.text('users:')
+
+        for (const user of users) {
+          document.text(`name: ${user.getName()}`)
+          document.text(`linkedIn: ${user.getProfile().getLinkedInUrl()}`)
+        }
+
+        document.end()
+
+        stream.on('finish', () => {
           const buffer = readFileSync(pathFile)
           unlinkSync(pathFile)
 
           return resolve(buffer)
         })
+      } catch (err) {
+        unlinkSync(pathFile)
+        reject(err)
+      }
     })
   }
 }
